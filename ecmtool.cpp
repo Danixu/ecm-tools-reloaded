c7f82ada783522c131c38de2f4619fd97892e469
Daniel Carrasco <danielmadrid19@gmail.com>
2021-07-15 08:08:04 +0200

    Added extreme compresison for lzma

--- ecmtool.cpp
+++ ecmtool.cpp
@@ -24,8 +24,8 @@
 #include "common.h"
 #include "banner.h"
 #include "sector_tools.h"
-#include "getopt.h"
-#include "stdbool.h"
+#include <getopt.h>
+#include <stdbool.h>
 #include <string>
 #include <stdexcept>
 #include "zlib.h"
@@ -62,6 +62,7 @@ static int8_t ecmify(
     sector_tools_compression data_compression,
     sector_tools_compression audio_compression,
     uint8_t compression_level,
+    bool extreme_compression,
     bool seekable,
     uint8_t sectors_per_block
 );
@@ -108,6 +109,7 @@ static struct option long_options[] = {
     {"acompression", required_argument, NULL, 'a'},
     {"dcompression", required_argument, NULL, 'd'},
     {"clevel", required_argument, NULL, 'c'},
+    {"extreme-compression", no_argument, NULL, 'e'},
     {"seekable", no_argument, NULL, 's'},
     {"sectors_per_block", required_argument, NULL, 'p'},
     {"force", required_argument, NULL, 'f'},
@@ -122,6 +124,7 @@ int main(int argc, char** argv) {
     sector_tools_compression audio_compression = C_NONE;
     sector_tools_compression data_compression = C_NONE;
     uint8_t compression_level = 5;
+    bool extreme_compression = false;
     bool force_rewrite = false;
     // ZLIB will be seekable
     bool seekable = false;
@@ -135,7 +138,7 @@ int main(int argc, char** argv) {
     const char *errstr;
 
     char ch;
-    while ((ch = getopt_long(argc, argv, "i:o:a:d:c:sp:f", long_options, NULL)) != -1)
+    while ((ch = getopt_long(argc, argv, "i:o:a:d:c:esp:f", long_options, NULL)) != -1)
     {
         // check to see if a single character or long option came through
         switch (ch)
@@ -202,6 +205,11 @@ int main(int argc, char** argv) {
                     return 1;
                 }
 
+                break;
+
+            // short option '-e', long option "--extreme-compresison" (only LZMA)
+            case 'e':
+                extreme_compression = true;
                 break;
 
              // short option '-s', long option "--seekable"
@@ -330,6 +338,7 @@ int main(int argc, char** argv) {
             data_compression,
             audio_compression,
             compression_level,
+            extreme_compression,
             seekable,
             sectors_per_block
         );
@@ -344,6 +353,7 @@ static int8_t ecmify(
     sector_tools_compression data_compression,
     sector_tools_compression audio_compression,
     uint8_t compression_level,
+    bool extreme_compression,
     bool seekable,
     uint8_t sectors_per_block
 ) {
@@ -618,10 +628,17 @@ static int8_t ecmify(
         }
 
         if (streams_toc[streams_toc_actual].compression && !compobj) {
+            uint8_t compression_option = compression_level;
+            if (
+                (sector_tools_compression)streams_toc[streams_toc_actual].compression == C_LZMA &&
+                extreme_compression    
+            ) {
+                compression_option |= LZMA_PRESET_EXTREME;
+            }
             compobj = new compressor(
                 (sector_tools_compression)streams_toc[streams_toc_actual].compression,
                 true,
-                compression_level
+                compression_option
             );
             compobj -> set_output(comp_buffer, buffer_size);
         }
@@ -1022,7 +1039,6 @@ static int8_t unecmify(
     return return_code;
 }
 
-
 void print_help() {
     banner();
     printf(
@@ -1037,10 +1053,14 @@ void print_help() {
         "    ecmtool -i/--input ecmfile -o/--output cdimagefile\n"
         "\n"
         "Optional options:\n"
-        "    -d/--dcompression <zlib>\n"
+        "    -a/--acompression <zlib/lzma>\n"
+        "           Enable audio compression\n"
+        "    -d/--dcompression <zlib/lzma>\n"
         "           Enable data compression\n"
         "    -c/--clevel <0-9>\n"
         "           Compression level between 0 and 9"
+        "    -e/--extreme-compression\n"
+        "           Enables extreme compression mode for LZMA (very slow)\n"
         "    -s/--seekable\n"
         "           Create a seekable file. Reduce the compression ratio but\n"
         "           but allow to seek into the stream.\n"
